19d7628e8730a2c458f4c2fb479043904479f5ff
James Papademas <jpapadem@iit.edu>
2024-11-29 18:03:48 -0600

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'jp',
         'HOST': '127.0.0.1',
-        'PORT': '3307' #'3307' if os.getenv('CI') else '3306',
+        'PORT': '3307' if os.getenv('CI') else '3306',
     }
 }
 
